4b860add641321668c11b293ae4e53d91574d65d
anna-zau <annazauholnikova555@gmail.com>
2022-10-12 15:28:08 +0300

    added 1-3 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery'); // link to Gallery
+
+//  creating a markup
+const markup = galleryItems.reduce(
+  (acc, item) =>
+    acc +
+    `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`,
+  ''
+);
+
+function render(markup) {
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+console.log(render(markup));
+
+gallery.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+//  Напиши скрипт который будет сохранять текущее время воспроизведения видео
+//  в локальное хранилище и,
+// при перезагрузке страницы, продолжать воспроизводить видео с этого времени.
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME = 'videoplayer-current-time';
+
+player.on('play', function (data) {
+  console.log('played the video!');
+  //   console.log(data);
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', throttle(saveVideoStopTime, 1000));
+
+function saveVideoStopTime(data) {
+  localStorage.setItem(CURRENT_TIME, data.seconds);
+}
+
+function setVideoTime() {
+  if (!localStorage.getItem(CURRENT_TIME)) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+}
+
+console.log(setVideoTime());
